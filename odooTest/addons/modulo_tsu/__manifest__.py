383d6da99fe3f11c5136d256d19c9a8e6ffb07e9
Gerson-Solano23 <159967377+Gerson-Solano23@users.noreply.github.com>
2025-02-03 01:20:04 -0600

    feat  v

--- odooTest/addons/modulo_tsu/__manifest__.py
+++ odooTest/addons/modulo_tsu/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "modulo_tsu",
+    'name': "Academia",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Gestión de cursos académicos",
 
     'description': """
 Long description of module's purpose
@@ -14,7 +14,7 @@ Long description of module's purpose
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Education',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -23,13 +23,9 @@ Long description of module's purpose
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+         'views/curso_views.xml'
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
     'application': True,
     'installable': True,
 }
--- odooTest/addons/modulo_tsu/models/models.py
+++ odooTest/addons/modulo_tsu/models/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from django.db.models.expressions import result
 from odoo import models, fields, api
 
 
@@ -16,4 +16,33 @@ class modulo_tsu(models.Model):
     def _value_pc(self):
         for record in self:
             record.value2 = float(record.value) / 100
+
+class Curso(models.Model):
+    _name = "modulo_tsu.curso"
+    _description = "Modelo para gestionar cursos académicos"
+    idCurso = fields.Char(
+        string="Código del Curso",
+        required=True,
+        size = 6,
+        unique = True
+    )
+    nombre = fields.Char(
+        string="Nombre del curso",
+        required=True,
+        size=60
+    )
+    creditos = fields.Integer(
+        string="Creditos",
+        required=True
+    )
+    
+    _sql_constrains = [
+        ('idCurso', 'UNIQUE(idCurso)', 'El id debe ser unico')
+    ]
+
+    def name_get(self):
+        result = []
+        for curso in self:
+            result.append((curso.idCurso, f"{curso.nombre} ({curso.creditos})"))
+        return result
 
